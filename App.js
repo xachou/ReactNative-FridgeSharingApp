137390cb1e18840d8eacbff33e5072dd55d15841
Andy <andyzhou@umich.edu>
2019-11-22 11:43:03 -0500

    Created firebase project and added the config

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAK4Tn8HNnIbrdfh5e1uR5JECM3T4J99N0",
+  authDomain: "finalproj-fridge.firebaseapp.com",
+  databaseURL: "https://finalproj-fridge.firebaseio.com",
+  projectId: "finalproj-fridge",
+  storageBucket: "finalproj-fridge.appspot.com",
+  messagingSenderId: "601009921612",
+  appId: "1:601009921612:web:fc4ecc77b48d4cd6409ee9"
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
